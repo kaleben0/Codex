e73dcbbe76b8bd170cfe9f28be2651bbc1bb7ca9
kaleben0 <52037180+kaleben0@users.noreply.github.com>
2019-10-20 12:37:32 -0400

    Update

---
+++ enhancements/enhance.js
@@ -0,0 +1,336 @@
+// JavaScript Document
+
+function toggleaccuracy() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function toggleaccuracy_debuff() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}
+}
+
+function toggleconfusion() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}
+}
+
+function toggledamage() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}
+}
+
+function toggledefence_buff() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}
+}
+
+function toggledefence_debuff() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}
+}
+
+function toggledisorient() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function toggleendurance_modification() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function toggleendurance_reduction() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function togglefear() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function togglefly () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function toggleheal () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function togglehold () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function toggleimmobilization () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function toggleintangibility () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function toggleinterrupt () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function togglejump () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function toggleknockback () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function togglerange () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #recharge, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+function togglerecharge () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #resist_damage, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function toggleresist_damage () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #run, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function togglerun () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #sleep, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function togglesleep () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #slow, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function toggleslow () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #taunt, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function toggletaunt () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #tohit_buff,#enhance_container").toggle();
+}}
+
+
+function toggletohit_buff () {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt,#enhance_container").toggle();
+}}
+
+
+
+
+function togglemm() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#natural_magic, #natural_technology, #science_technology, #mutation_science, #mutation, #magic, #natural, #science, #technology").toggle();
+}}
+
+
+function togglenm() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_technology, #science_technology, #mutation_science, #mutation, #magic, #natural, #science, #technology").toggle();
+}}
+
+function togglent() {
+if (event.altKey) {
+ location.reload();
+}
+else{ 
+$("#magic_mutation, #natural_magic, #science_technology, #mutation_science, #mutation, #magic, #natural, #science, #technology").toggle();
+}}
+
+
+
+function togglest() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #mutation_science, #mutation, #magic, #natural, #science, #technology").toggle();
+}}
+
+
+
+function togglems() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation, #magic, #natural, #science, #technology").toggle();
+}}
+
+
+
+
+function togglemutation() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation_science, #magic, #natural, #science, #technology").toggle();
+}}
+
+
+
+function togglemagic() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation_science, #mutation, #natural, #science, #technology").toggle();
+}}
+
+
+
+
+
+function togglenatural() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation_science, #mutation, #magic, #science, #technology").toggle();
+}}
+
+
+
+
+
+function togglescience() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation_science, #mutation, #magic, #natural, #technology").toggle();
+}}
+
+
+
+
+
+
+function toggletechnology() {
+if (event.altKey) {
+ location.reload();
+}
+else {
+$("#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation_science, #mutation, #magic, #natural, #science").toggle();
+}}
+
+
+function clear_page () {
+$('#magic_mutation, #natural_magic, #natural_technology, #science_technology, #mutation_science, #mutation, #magic, #natural, #science, #technology,#accuracy, #accuracy_deBuff, #confusion, #damage, #defence_buff, #defence_debuff, #disorient, #endurance_modification, #endurance_reduction, #fear, #fly, #heal, #hold, #immobilization, #intangibility, #interrupt, #jump, #knockback, #range, #recharge, #resist_damage, #run, #sleep, #slow, #taunt,#tohit_buff,#enhance_container').show();
+}
+
