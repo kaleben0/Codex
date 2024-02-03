96ad481e57b09a64de4fa6aed822d0e2e5d2f0ec
Douglas Redman <douglas.redman2@gmail.com>
2024-02-03 18:10:33 -0500

    modified index.php in enhancements

--- enhancements/index.php
+++ enhancements/index.php
@@ -19,7 +19,6 @@
       <link rel="icon" sizes="192x192" href="../images/icons/icon-192x192.png">
       <link rel="icon" sizes="384x384" href="../images/icons/icon-384x384.png">
       <link rel="icon" sizes="512x512" href="../images/icons/icon-512x512.png">
-      <link rel="stylesheet" type="text/css" href="../codex.css">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
@@ -32,7 +31,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
       </script>
-
+      <link rel="stylesheet" type="text/css" href="../codex.css">
       <script src="../enhancements/enhance.js"></script>
       <script language="javascript" type="text/javascript">
       var images = new Array()
