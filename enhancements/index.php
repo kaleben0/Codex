5d36742cb30da446171fdd4a89d73ac9a791d349
Douglas Redman <douglas.redman2@gmail.com>
2024-02-04 18:59:06 -0500

    modified index.php in enhancements

--- enhancements/index.php
+++ enhancements/index.php
@@ -109,7 +109,7 @@
         var iconElement = document.getElementById('copylist');
         iconElement.classList.remove('fa-copy');
         iconElement.classList.add('fa-square-check');
-        setTimeout(myFunction, 15000);
+        await new Promise(resolve => setTimeout(resolve, 15000));
         iconElement.classList.remove('fa-square-check');
         iconElement.classList.add('fa-copy');
     }
