0281507b2ed0c9090249c27abcee8c448f662016
Douglas Redman <douglas.redman2@gmail.com>
2024-02-04 18:31:01 -0500

    modified index.php in enhancements

--- enhancements/index.php
+++ enhancements/index.php
@@ -118,7 +118,7 @@
     <hr />
     <div class="page_header" onclick="location.reload();">Enhancement Shopping List</div>
     <hr />
-    <div style="max-width:500px;" class="mx-auto">
+    <div style="max-width: 450px;" class="mx-auto">
         <div class="sub_header_enhance">List</div>
         <div class="list_container">
             <ul id="the_list">
